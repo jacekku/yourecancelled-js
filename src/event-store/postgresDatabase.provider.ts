426d131bf25aff2a19a878c4d631c43c86f106cd
jacekku <jacekwyroba97@gmail.com>
2024-11-17 10:38:11 +0100

    More options for DB connection

--- src/event-store/postgresDatabase.provider.ts
+++ src/event-store/postgresDatabase.provider.ts
@@ -1,14 +1,15 @@
+import { ConfigService } from '@nestjs/config';
 import { DataSource } from 'typeorm';
 import { EventEntity } from './Event.entity';
-import { ConfigService } from '@nestjs/config';
 
-export const databaseProvider = (config: ConfigService) => new DataSource({
-  type: 'postgres',
-  host: 'localhost',
-  port: 25432,
-  username: 'eventstore',
-  password: config.get('DB_PASSWORD'), 
-  database: 'eventstore',
-  entities: [EventEntity],
-  migrations: [__dirname + '/../**/migrations/*{.ts,.js}'],
-});
+export const databaseProvider = (config: ConfigService) =>
+  new DataSource({
+    type: 'postgres',
+    host: config.get('DB_HOST') || 'localhost',
+    port: config.get('DB_PORT') || 25432,
+    username: 'eventstore',
+    password: config.get('DB_PASSWORD'),
+    database: 'eventstore',
+    entities: [EventEntity],
+    migrations: [__dirname + '/../**/migrations/*{.ts,.js}'],
+  });
